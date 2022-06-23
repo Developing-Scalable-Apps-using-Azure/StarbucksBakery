a80b852018da2dcc4bfb482a5212cc136b05ee69
siddharth-dhawan <siddharthdwn@gmail.com>
2022-06-23 17:14:57 +0000

    Add project files.

---
+++ internationalbakers/Data/ibsql01Context.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using internationalbakers.Models;
+
+#nullable disable
+
+namespace internationalbakers.Data
+{
+    public partial class ibsql01Context : DbContext
+    {
+        public ibsql01Context()
+        {
+        }
+
+        public ibsql01Context(DbContextOptions<ibsql01Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cookie> Cookies { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderLine> OrderLines { get; set; }
+        public virtual DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //optionsBuilder.UseSqlServer("Server=tcp:ibsqlserv01.database.windows.net,1433;Initial Catalog=ibsql01;Persist Security Info=False;User ID=adminuser;Password=Admin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Cookie>(entity =>
+            {
+                entity.Property(e => e.Name).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.StoreId)
+                    .HasConstraintName("StoreForeignKey");
+            });
+
+            modelBuilder.Entity<OrderLine>(entity =>
+            {
+                entity.HasOne(d => d.Cookie)
+                    .WithMany(p => p.OrderLines)
+                    .HasForeignKey(d => d.CookieId)
+                    .HasConstraintName("CookieForeignKey");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderLines)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("OrderForeignKey");
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.Property(e => e.Country).IsUnicode(false);
+
+                entity.Property(e => e.Name).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
